f4badd78d95caa262ced54629a4efe53518d8482
Tauane Sales <taysales6@gmail.com>
2024-04-27 20:28:31 -0300

    fix: vercel deployment

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ import db from "./src/database/configdb.js";
 import express from "express";
 import userRoute from "./src/routes/user.route.js";
 import rootRoute from "./src/routes/root.route.js";
-import swaggerRoute from "./src/routes/swagger.route.js";
+import swaggerRoute from "./src/routes/swagger/swagger.route.js";
 
 const app = express();
 
 app.use(express.json());
 app.use("/user", userRoute);
 app.use("/", rootRoute);
-app.use("/doc", swaggerRoute);
+app.use("/docs", swaggerRoute);
 
 db.connect();
 
