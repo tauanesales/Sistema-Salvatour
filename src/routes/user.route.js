fc7c98a9d36bb68cf497b52b1c42b0ca6f6f7d94
EvertonWallace <everton542@hotmail.com>
2024-05-13 21:29:34 -0300

    criacao da rota para trazer todos os usuarios

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -4,6 +4,7 @@ const route = express.Router();
 import {validId, validUser} from "../middlewares/global.middlewares.js";
 
 route.post('/', userController.create);
+route.get('/',userController.findAll);
 route.get('/:id', validId, validUser ,userController.findById);
 route.patch("/:id", validId, validUser ,userController.update);
 
