4247b9da3b157c3d30c28bb26a234f13afa7c29a
JoandersonG <joandersongsantos@yahoo.com>
2024-05-19 12:04:58 -0300

    Including  JWT into protected routes + returning JWT token in login

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ import db from "./src/database/configdb.js";
 import express from "express";
 import userRoute from "./src/routes/user.route.js";
 import rootRoute from "./src/routes/root.route.js";
+import loginRoute from "./src/routes/auth.route.js";
 import swaggerRoute from "./src/routes/swagger.route.js";
+import cors from 'cors';
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use("/user", userRoute);
+app.use("/auth", loginRoute);
 app.use("/", rootRoute);
 app.use("/doc", swaggerRoute);
 
