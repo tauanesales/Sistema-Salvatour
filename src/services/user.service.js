3f665ff4031c1956c2de53640fd0f95005717233
EvertonWallace <everton542@hotmail.com>
2024-05-21 16:16:49 -0300

    criacao da funcao deleteUser e ajustes de bugs

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -2,45 +2,23 @@ import User from "../models/User.js";
 
 const create = (body) => User.create(body);
 
-const findAll = () => User.find();
+const findByIdService = (id) => User.findOne({ _id: id });
 
-const findByIdService = (id) => User.findOne({ id });
+const deleteUser = (id) => User.findByIdAndDelete(id);
 
-const deleteUserById = async (requestingUserId, userIdToDelete) => {
-    try {
-      const requestingUser = await User.findById(requestingUserId);
-      
-      if (!requestingUser.isAdmin) {
-        throw new Error('Usuário não tem permissão para excluir outros usuários');
-      }
-  
-      const userToDelete = await User.findByIdAndDelete(userIdToDelete);
-  
-      if (!userToDelete) {
-        throw new Error('Usuário não encontrado');
-      }
-  
-      return { message: 'Usuário deletado com sucesso' };
-    } catch (error) {
-      throw new Error(`Erro ao excluir usuário: ${error.message}`);
+const updateService = (id, name, email, password) =>
+  User.findOneAndUpdate(
+    { _id: id },
+    {
+      name,
+      email,
+      password,
     }
-  };
-
-const updateService = (
-    id,
-    name,
-    email,
-    password
-) => User.findOneAndUpdate(
-    {_id: id},{
-    name,
-    email,
-    password});
+  );
 
 export default {
-    create,
-    findAll,
-    findByIdService,
-    updateService,
-    deleteUserById
+  create,
+  findByIdService,
+  updateService,
+  deleteUser,
 };
