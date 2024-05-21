2c77c53205f7ca6183ff04bb0b3dabece219a702
JoandersonG <joandersongsantos@yahoo.com>
2024-05-20 21:04:26 -0300

    Desativando temporariamente o middleware para Validar token

--- src/routes/root.route.js
+++ src/routes/root.route.js
@@ -2,7 +2,7 @@ import express from 'express';
 import {validToken} from "../middlewares/jwt.token.middleware.js";
 const route = express.Router();
 
-route.get('/', validToken, (req, res) => {
+route.get('/', /* validToken, */ (req, res) => {
     res.send('Hello World! It works!')
   })
 
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -5,10 +5,10 @@ import {validId, validUser} from "../middlewares/global.middlewares.js";
 import {validToken} from "../middlewares/jwt.token.middleware.js";
 
 route.post('/', userController.create);
-route.get('/', validToken, userController.findAll);
-route.get('/:id', validId, validUser, validToken, userController.findById);
-route.patch("/:id", validId, validUser, validToken, userController.update);
-route.delete('/:id', validToken, userController.deleteUserById);
+route.get('/', /* validToken, */ userController.findAll);
+route.get('/:id', validId, validUser, /* validToken, */ userController.findById);
+route.patch("/:id", validId, validUser, /* validToken, */ userController.update);
+route.delete('/:id', /* validToken, */ userController.deleteUserById);
 
 
 export default route;
