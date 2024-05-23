335446fc17ab7483e129223f4754f7054b3fb86b
EvertonWallace <everton542@hotmail.com>
2024-05-22 23:42:24 -0300

    criacao da funcao updatePasswordService

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -18,10 +18,29 @@ const updateService = (id, name, email, password) =>
     }
   );
 
+  const updatePasswordService = async (email, password) => {
+    try {
+      const updatedUser = await User.findOneAndUpdate(
+        { email: email },
+        { password: password },
+        { new: true, runValidators: true }
+      );
+  
+      if (!updatedUser) {
+        throw new Error('User not found');
+      }
+  
+      return updatedUser;
+    } catch (error) {
+      throw error;
+    }
+  };
+
 export default {
   create,
   findByIdService,
   updateService,
   deleteUser,
-  findByEmailService
+  findByEmailService,
+  updatePasswordService
 };
