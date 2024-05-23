6138ea82f50f8d0f88a095011d6ebf8058e84218
EvertonWallace <everton542@hotmail.com>
2024-05-22 23:44:15 -0300

    inclusao do campo cityAndState

--- src/models/User.js
+++ src/models/User.js
@@ -14,6 +14,10 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: true   
     },
+    cityAndState: {
+        type: String,
+        required: true   
+    },
     isAdmin: {
         type: Boolean
     }
