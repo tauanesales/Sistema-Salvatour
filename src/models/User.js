7d60d32532bff9d61ead801713f4b660343c41e9
AlbertoDoc <albertooliveirasantos@gmail.com>
2024-05-25 01:19:24 -0300

    fix: Corrigindo erro na comparação de senhas

--- src/models/User.js
+++ src/models/User.js
@@ -13,7 +13,7 @@ const UserSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true,
-        select: false   
+        select: true   
     },
     cityAndState: {
         type: String,
