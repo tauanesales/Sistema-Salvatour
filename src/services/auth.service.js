3e88991b1e580207d11f1f13d496fc2d886c5da2
EvertonWallace <everton542@hotmail.com>
2024-05-25 00:51:58 -0300

    modificacao do nome do authService para loginService

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,8 +1,8 @@
 import User from "../models/User.js";
 import jwt from 'jsonwebtoken'
 
-const loginService = (email) => User.findOne({ email });
+const loginService = (email) => User.findOne({ email }).select("+password");
 
 const generateToken = (id) => jwt.sign({id: id}, process.env.SECRET_JWT_KEY, { expiresIn: '1h' });
 
-export { authService, generateToken };
+export { loginService, generateToken };
