59b6d221b87f33926dcad370dfc2f49bb1f3a81b
EvertonWallace <everton542@hotmail.com>
2024-05-25 00:51:20 -0300

    remocao de 4 rotas

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -5,13 +5,9 @@ import {validToken} from "../middlewares/jwt.token.middleware.js";
 
 const route = express.Router();
 
-route.post('/', userController.create);
 route.get('/:id', validId, validUser, userController.findById);
 route.patch("/:id", validId, validUser, userController.update);
 route.delete('/:id', userController.deleteUser);
-route.post('/check-mail', userController.checkMail);
-route.get('/verify-token/:token', userController.verifyToken);
-route.post('/modify-password', userController.modifyPassword);
 
 // route.get('/', validToken, userController.findAll);
 // route.get('/:id', validId, validUser, validToken, userController.findById);
