9eebe76d5a3ca6a8a2e416cb9f00e1e0e6752001
EvertonWallace <everton542@hotmail.com>
2024-05-25 00:48:56 -0300

    implementacao do try/catch

--- src/middlewares/jwt.token.middleware.js
+++ src/middlewares/jwt.token.middleware.js
@@ -1,22 +1,26 @@
-
-import jwt from'jsonwebtoken'
+import jwt from "jsonwebtoken";
 
 export const validToken = (req, res, next) => {
+  try {
     let token = req.headers.authorization;
-  
+
     if (!token) {
-      return res.status(401).json({ message: 'Token not provided' });
+      return res.status(401).json({ message: "Token not provided" });
     }
-  
-    token = token.replace('Bearer ', '')
-  
-    jwt.verify(token,  process.env.SECRET_JWT_KEY, (err, decoded) => {
+
+    token = token.replace("Bearer ", "");
+
+    jwt.verify(token, process.env.SECRET_JWT_KEY, (err, decoded) => {
       if (err) {
-        console.error('JWT verify error:', err)
-        return res.status(403).json({ message: 'Invalid token' });
+        console.error("JWT verify error:", err);
+        return res.status(403).json({ message: "Invalid token" });
       }
-  
-      req.usuario = decoded; 
+
+      req.usuario = decoded;
       next();
     });
-  };
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
