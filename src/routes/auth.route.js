979f1ab2c3ecbe8f851c4bd0a10eafe028350ec8
EvertonWallace <everton542@hotmail.com>
2024-05-25 00:49:47 -0300

    criacao de 4 novas rotas, register, check-mail, modify-password, verify-token

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,10 +1,18 @@
 import authController from '../controller/auth.controller.js';
+import userController from '../controller/user.controller.js';
+
 import express from 'express';
 import cors from 'cors';
+import { validEmail } from '../middlewares/global.middlewares.js';
 const app = express();
 app.use(cors());
 const route = express.Router();
 
-route.post('/', authController.authenticate);
+route.post('/login', authController.authenticate);
+route.post('/register', authController.register);
+route.post('/check-mail', validEmail, authController.sendMailController);
+route.get('/verify-token/:token', authController.verifyToken);
+route.post('/modify-password', authController.modifyPassword);
+
 
 export default route;
