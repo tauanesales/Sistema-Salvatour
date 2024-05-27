73bf5d9cb7eb07133c73235b3c37af9c155fdde7
Tauane Sales <62819445+tauanesales@users.noreply.github.com>
2024-05-27 15:01:03 -0300

    Update auth.route.js

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -2,7 +2,6 @@ import authController from '../controller/auth.controller.js';
 import express from 'express';
 import cors from 'cors';
 import { validEmail } from '../middlewares/global.middlewares.js';
-import { validToken } from "../middlewares/jwt.token.middleware.js";
 
 
 const app = express();
@@ -13,7 +12,7 @@ const route = express.Router();
 route.post('/login', authController.authenticate);
 route.post('/register', authController.register);
 route.post('/check-mail', validEmail, authController.sendMailController);
-route.get('/verify-token/', validToken, authController.verifyToken);
+route.get('/verify-token/:token', authController.verifyToken);
 route.post('/modify-password', authController.modifyPassword);
 
 
