a4d73b861b3fc3e8107e77ecd1443c3ff992f37a
EvertonWallace <everton542@hotmail.com>
2024-06-21 20:10:08 -0300

    modificacao na rota create para armazenar a imagem enviada pelo usuario na pasta temporaria uploads

--- src/routes/touristAttraction.route.js
+++ src/routes/touristAttraction.route.js
@@ -3,14 +3,17 @@ import express from 'express';
 import cors from 'cors';
 import { validToken } from '../middlewares/jwt.token.middleware.js';
 import { validId } from "../middlewares/global.middlewares.js";
+import multer from 'multer';
+
 
 const app = express();
+const upload = multer({ dest: 'uploads/' });
 app.use(cors());
 
 const route = express.Router();
 route.get('/all', validToken, touristAttractionController.getAttractions);
 route.patch('/:id', validToken, validId, touristAttractionController.updateAttraction);
 route.delete('/:id', validToken, validId, touristAttractionController.deleteAttraction);
-route.post('/create', validToken, touristAttractionController.addAttraction);
+route.post('/create', validToken, upload.single('image'), touristAttractionController.addAttraction);
 
 export default route;
