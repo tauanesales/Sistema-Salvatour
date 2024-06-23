9138905596a1a5b5eedfce579d9fc3e9b64ab475
EvertonWallace <everton542@hotmail.com>
2024-06-23 11:16:22 -0300

    inclusao do middleware do multer na rota patch

--- src/routes/touristAttraction.route.js
+++ src/routes/touristAttraction.route.js
@@ -12,7 +12,7 @@ app.use(cors());
 
 const route = express.Router();
 route.get('/all', validToken, touristAttractionController.getAttractions);
-route.patch('/:id', validToken, validId, touristAttractionController.updateAttraction);
+route.patch('/:id', validToken, validId, upload.single('image'),  touristAttractionController.updateAttraction);
 route.delete('/:id', validToken, validId, touristAttractionController.deleteAttraction);
 route.post('/create', validToken, upload.single('image'), touristAttractionController.addAttraction);
 
