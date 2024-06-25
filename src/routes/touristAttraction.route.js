ec839d83e6e9eee3c46dc86c11168964c6cf44a6
EvertonWallace <everton542@hotmail.com>
2024-06-25 14:11:58 -0300

    criacao da rota getImage

--- src/routes/touristAttraction.route.js
+++ src/routes/touristAttraction.route.js
@@ -7,13 +7,15 @@ import multer from 'multer';
 
 
 const app = express();
-const upload = multer({ dest: 'uploads/' });
+const upload = multer({ dest: 'images/' });
 app.use(cors());
 
 const route = express.Router();
 route.get('/all', validToken, touristAttractionController.getAttractions);
+route.get('/image/:id', touristAttractionController.getImage);
 route.patch('/:id', validToken, validId, upload.single('image'),  touristAttractionController.updateAttraction);
 route.delete('/:id', validToken, validId, touristAttractionController.deleteAttraction);
 route.post('/create', validToken, upload.single('image'), touristAttractionController.addAttraction);
+
 
 export default route;
